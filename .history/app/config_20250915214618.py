cfec9a1c891914f6d5df37e2a48bb5b40e83e9f5
Kaustubh Keny <Kaustubh.Keny@Cogencis.com>
2025-09-16 09:01:10 +0530

    history removed

--- .history/app/config_20250915214618.py
+++
@@ -1,9 +0,0 @@
-from app.utils import Helper
-from datetime import datetime
-
-TODAY = datetime.now().strftime("%Y%m%d")
-CONFIG_PATH = r"config\params.json5"
-CONFIGS = Helper.load_json5(CONFIG_PATH)
-
-LOG_DIR =""
-SAVE_DIR = ""
